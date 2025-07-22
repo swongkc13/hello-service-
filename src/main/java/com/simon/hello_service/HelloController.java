17e85bf62e9ae92b231b7c2e1d30feb56aa5a157
swongkc13 <simon.wong@recyclewell.nl>
2025-07-22 14:48:42 +0200

    Day 1: Spring Boot Hello Service + Docker

---
+++ src/main/java/com/simon/hello_service/HelloController.java
@@ -0,0 +1,11 @@
+package com.simon.hello_service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+}
---
+++ src/main/java/com/simon/hello_service/HelloServiceApplication.java
@@ -0,0 +1,13 @@
+package com.simon.hello_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServiceApplication.class, args);
+	}
+
+}
